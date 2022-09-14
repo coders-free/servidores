a4a254a49216fc1742caa6f16c2975bba6a5b46f
Victor Arana <victor@codersfree.com>
2022-09-14 12:22:29 -0500

    actualizacion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <h1>Prueba de deploy al hacer un push</h1>
+                <h1>Prueba 2 de deploy al hacer un push</h1>
             </div>
         </div>
     </div>
